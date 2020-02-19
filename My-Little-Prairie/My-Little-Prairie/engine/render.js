40b48e8a3dd0191257d267101b2cabb8d5042207
LJ <ljenloe@live.unc.edu>
2020-02-19 11:47:05 -0500

    added planting

--- My-Little-Prairie/My-Little-Prairie/engine/render.js
+++ My-Little-Prairie/My-Little-Prairie/engine/render.js
@@ -9,6 +9,7 @@ export const renderGame = function(game) {
 
     let string = `<h2>My Little Prairie</h2>
     <button id="weed">Weed</button>
+    <button id="plant">Plant</button>
     <ul id="hexGrid">`;
     for (let i = 0; i < 59; i++){
         
@@ -24,8 +25,19 @@ export const renderGame = function(game) {
     </li>`
 
         }
-        else {
+        else if(tileState[i]==0) {
         string+=`<li class="hex">
+        <div class="hexIn">
+        <a class="hexLink weedTile" href="#" onclick="popup(this)">
+            <img src="pics/blankTile.jpg" alt="" />
+            <h1>Plant Name Here</h1>
+            <p>Description of plant</p>
+        </a>
+        </div>
+    </li>`;
+        }
+        else {
+            string+=`<li class="hex">
         <div class="hexIn">
         <a class="hexLink weedTile" href="#" onclick="popup(this)">
             <img src="pics/grass.jpg" alt="" />
@@ -39,6 +51,7 @@ export const renderGame = function(game) {
     string+=`</ul>`
     const $root = $('#root');
     $root.on('click', '#weed', handleWeedButtonPress);
+    $root.on('click', '#plant', handlePlantButtonPress);
     return string;
 
 }
@@ -51,6 +64,18 @@ export const handleWeedButtonPress = function(event) {
     $root.empty();
     $root.append(renderSite());
     $root.append(renderWeedingBoard(game));
+    
+
+}
+
+export const handlePlantButtonPress = function(event) {
+
+    //alert("button pressed");
+    let game = new Game(59);
+    const $root = $('#root');
+    $root.empty();
+    $root.append(renderSite());
+    $root.append(renderPlantingBoard(game));
     
 
 }
@@ -81,8 +106,21 @@ export const renderWeedingBoard = function(game) {
     </li>`
     $root.on('click', '#'+idString, handleWeedActionClick);
         }
-        else {
+        else if(tileState[i]==0){
         string+=`<li class="hexWeed" id="${idString} data-id="${idString}">
+        <div class="hexIn">
+        <a class="hexLink" href="#" id="${idString}" data-id="${idString}">
+            <img src="pics/blankTile.jpg" alt="" />
+            <h1>Remove Weed</h1>
+            <p></p>
+        </a>
+        </div>
+    </li>`;
+    $root.on('click', '#'+idString, handleWeedActionClick);
+    //weedCount++;
+        }
+        else {
+            string+=`<li class="hexWeed" id="${idString} data-id="${idString}">
         <div class="hexIn">
         <a class="hexLink" href="#" id="${idString}" data-id="${idString}">
             <img src="pics/grass.jpg" alt="" />
@@ -92,7 +130,6 @@ export const renderWeedingBoard = function(game) {
         </div>
     </li>`;
     $root.on('click', '#'+idString, handleWeedActionClick);
-    //weedCount++;
         }
     }
     string+=`</ul>`;
@@ -100,6 +137,67 @@ export const renderWeedingBoard = function(game) {
     $root.on('click', '#finish', handleFinishWeedingButtonPress);
 
     return string;
+
+}
+
+export const renderPlantingBoard = function(game) {
+
+    const $root = $('#root');
+    let board = game.gameState.board;
+    let score = game.gameState.score;
+    
+    
+    let string = `<h2>My Little Prairie</h2>
+    <button id="finish">Finish Planting</button>
+    <ul id="hexGrid">`;
+    for (let i = 0; i < 59; i++){
+        //identifier=i;
+        let idString=i;
+
+        if(tileState[i]==1) {
+            string+=`<li class="hexPlant" id="${idString} data-id="${idString}">
+        <div class="hexIn">
+        <a class="hexLink" href="#" id="${idString}" data-id="${idString}">
+            <img src="pics/weeds.jpg" alt="" />
+            <h1>Plant Here</h1>
+            <p></p>
+        </a>
+        </div>
+    </li>`
+    $root.on('click', '#'+idString, handlePlantActionClick);
+        }
+        else if(tileState[i]==0){
+        string+=`<li class="hexPlant" id="${idString} data-id="${idString}">
+        <div class="hexIn">
+        <a class="hexLink" href="#" id="${idString}" data-id="${idString}">
+            <img src="pics/blankTile.jpg" alt="" />
+            <h1>Plant Here</h1>
+            <p></p>
+        </a>
+        </div>
+    </li>`;
+    $root.on('click', '#'+idString, handlePlantActionClick);
+    //weedCount++;
+        }
+        else {
+            string+=`<li class="hexPlant" id="${idString} data-id="${idString}">
+        <div class="hexIn">
+        <a class="hexLink" href="#" id="${idString}" data-id="${idString}">
+            <img src="pics/grass.jpg" alt="" />
+            <h1>Uproot</h1>
+            <p></p>
+        </a>
+        </div>
+    </li>`;
+    $root.on('click', '#'+idString, handlePlantActionClick);
+        }
+    }
+    string+=`</ul>`;
+    
+    $root.on('click', '#finish', handleFinishWeedingButtonPress);
+
+    return string;
+
 
 }
 
@@ -118,6 +216,26 @@ export const handleWeedActionClick = function(event) {
     $root.off();
     
     handleWeedButtonPress();
+    
+
+}
+
+export const handlePlantActionClick = function(event) {
+    let currentTile = event.currentTarget.getAttribute("id");
+    console.log(currentTile);
+    if(tileState[currentTile]==1) {
+        
+        alert("Cannot plant on Tile " + currentTile + "!");
+    }
+    else {
+        tileState[currentTile]=2;
+        alert("Planted on Tile " + currentTile +".");
+    }
+
+    const $root = $('#root');
+    $root.off();
+    
+    handlePlantButtonPress();
     
 
 }
