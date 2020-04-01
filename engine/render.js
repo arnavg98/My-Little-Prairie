5499a4800a5c94f6f91da6a04636158f6bd4282a
theBowja <ericxin@gmail.com>
2020-03-31 22:08:55 -0400

    added log for gamestate for every action
    exposes three function to global (call these functions in console):
    getGameState(i)
    getActionCounter()
    setSubscribe(boolean);

--- engine/render.js
+++ engine/render.js
@@ -1,5 +1,7 @@
 import Game from "./game.js";
+
 import { plantdefs } from "../public/defs/plantdefs.js";
+import { addGameState } from "./actionslog.js";
 
 
 //for reference src="${plantdefs[tileState[i].name].image}" is how to refer to plant's image
@@ -400,6 +402,7 @@ export const handleWeedActionClick = function(event) {
         }
         //alert("Tile " + currentTile + " weeded!");
         actions = actions + 1;
+        logGameState();
         console.log(actions);
         if (actions % 2 == 0) {
             let i = Math.floor(Math.random() * 59);
@@ -505,6 +508,7 @@ export const handlePlantActionClick = function(event) {
 
         //score+=50*plantdefs[currentplant].growthrate;
         actions = actions + 1;
+        logGameState();
         console.log(actions);
         if (actions % 2 == 0) {
             let i = Math.floor(Math.random() * 59);
@@ -614,6 +618,21 @@ export const scoreUpdate = function() {
 
 
 }
+
+function logGameState() {
+    addGameState({
+        tileState: tileState,
+        actions: actions,
+        growthCounter: growthCounter,
+        weedCounter: weedCounter,
+        currenttool: currenttool,
+        currentplant: currentplant,
+        year: year,
+        season: season,
+        score: score,
+    })
+}
+
 export const handleSeason = function() {
     let string = ``;
     if (actions < 30*year) {
