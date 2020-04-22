68677de1b21f04d0924a3348d6aa579c511c4a00
arnavg98 <46693221+arnavg98@users.noreply.github.com>
2020-04-22 01:38:36 -0400

    Revert "Added Dormant pics"

--- engine/render.js
+++ engine/render.js
@@ -844,7 +844,6 @@ export function logGameState() {
         score: score,
     };
 }
-//addpoints is for events to be able to add to the score, dont touch it or you break events!
 export function addPoints(amount) {
     score+= amount;
 }
@@ -917,7 +916,7 @@ export const handleSeason = function(i) {
             string=`<li class="hex">
         <div class="hexIn">
         <a class="hexLink weedTile" href="#" onclick="popup(this)">
-            <img src="${plantdefs[tileState[i].name].dormant}" alt="" />
+            <img src="public/assets/dormant-grass-1.jpg" alt="" />
             <h1>${plantdefs[tileState[i].name].commonname}</h1>
             <p>${plantdefs[tileState[i].name].description}</p>
         </a>
